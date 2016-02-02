b764c9047bf4e44a4f52c6e118aeefe05ebaf3f1
yujie <yujie@corp.netease.com>
2016-02-02 14:14:25 +0800

    add local configuration user.el.

--- init.el
+++ init.el
@@ -11,6 +11,11 @@
 (add-sub-under-user-directory "lisp")
 (add-sub-under-user-directory "lisp/use-package")
 
+(let ((user-config-file (expand-file-name "user.el"
+					  user-emacs-directory)))
+  (if (file-exists-p user-config-file)
+      (load-file user-config-file)))
+
 (require 'settings)
 (settings-set-all)
 
