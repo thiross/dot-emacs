659023a2c176caa6e28ca0abbcd484a3c7cf07d7
yujie <yujie@corp.netease.com>
2016-02-24 14:57:36 +0800

    fix ghc missing bug.

--- init.el
+++ init.el
@@ -1,18 +1,19 @@
 ;; add subdirectories to load-path
 (defun add-sub-under-user-directory (path)
   (add-to-list 'load-path
-	       (expand-file-name path user-emacs-directory)))
+               (expand-file-name path user-emacs-directory)))
 
 (defun add-executable-path (path)
   (if (and path
-	   (not (member path exec-path)))
-      (add-to-list 'exec-path path)))
+           (not (member path exec-path)))
+      (progn (add-to-list 'exec-path path)
+             (setenv "PATH" (mapconcat 'identity exec-path ";")))))
 
 (add-sub-under-user-directory "lisp")
 (add-sub-under-user-directory "lisp/use-package")
 
 (let ((user-config-file (expand-file-name "user.el"
-					  user-emacs-directory)))
+                                          user-emacs-directory)))
   (if (file-exists-p user-config-file)
       (load-file user-config-file)))
 
