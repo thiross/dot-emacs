a4191563fc24ce5d344aea4537b62f19991b4693
俞洁 <yujie@corp.netease.com>
2016-10-13 13:38:49 +0800

    change default font.

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
-;; add subdirectories to load-path
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; add subdirectories to load-path
 (defun add-sub-under-user-directory (path)
   (add-to-list 'load-path
 	       (expand-file-name path user-emacs-directory)))
